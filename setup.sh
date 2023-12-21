1b39af943e2ec5752f1c93eb624ad28870c8f73d
Malin Markov <malin_id@yahoo.com>
2023-12-21 13:39:18 +0000

    Installing virtualenv from the setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-virtualenv --version
+virtualenv --version > /dev/null
 
 if [ $? != 0 ]
 then
-	echo "No virtualenv installed. RUN: pip install virtualenv"
-	exit 1
+	echo -e "No virtualenv installed. Installing virtualenv...\n\n"
+	sleep 3
+	pip3 install virtualenv
 fi
 
 virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+
+echo -e "\n\n\n *** Setup Completed! You can run the app now: ./app.py"
