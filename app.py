07354d55001f16404407eba67c0b1ed108e21373
Malin Markov <malin_id@yahoo.com>
2023-12-23 11:47:05 +0000

    update

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def swagger():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    print('* OS Port: ', port)
+    print('* OS Port:  ', port)
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
